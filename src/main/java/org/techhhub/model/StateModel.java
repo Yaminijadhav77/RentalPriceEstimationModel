c4019ce87c5ee745b908b8b7db11d183bfb8a8f4
YaminiJadhav77 <yamijadhav77@gmail.com>
2026-03-12 12:19:03 +0530

    Added remaining projetc files

--- src/main/java/org/techhhub/model/StateModel.java
+++ src/main/java/org/techhhub/model/StateModel.java
@@ -1,5 +1,6 @@
 package org.techhhub.model;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 public class StateModel {
@@ -8,7 +9,28 @@ public class StateModel {
    private UUID State_id;
    private String name;
    private String stateCode;
+   private String status;
+   private LocalDateTime Created_at;
+   public LocalDateTime getCreated_at() {
+	return Created_at;
+}
+   public void setCreated_at(LocalDateTime created_at) {
+	Created_at = created_at;
+   }
+   public LocalDateTime getUpdatedAt() {
+	return updatedAt;
+   }
+   public void setUpdatedAt(LocalDateTime updatedAt) {
+	this.updatedAt = updatedAt;
+   }
+   private LocalDateTime updatedAt;
    
+   public String getStatus() {
+	return status;
+}
+   public void setStatus(String status) {
+	this.status = status;
+   }
    public UUID getState_id() {
 	return State_id;
 }
@@ -30,10 +52,11 @@ public class StateModel {
    public StateModel() {
 	   
    }
-   public StateModel(UUID State_id, String name, String stateCode) {
+   public StateModel(UUID State_id, String name, String stateCode, String status) {
        this.State_id = State_id;
        this.name = name;
        this.stateCode = stateCode;
+       this.status=status;
    }
    
 }
